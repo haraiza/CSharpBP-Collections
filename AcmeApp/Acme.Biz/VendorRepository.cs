9905ea7f8c200bcd042c684da21566e1e6d8576a
haraiza <humbertoaraiza@gmail.com>
2019-03-20 13:34:07 -0700

    3_20_2019

--- AcmeApp/Acme.Biz/VendorRepository.cs
+++ AcmeApp/Acme.Biz/VendorRepository.cs
@@ -67,72 +67,67 @@ namespace Acme.Biz
             return success;
         }
 
-        public List<Vendor> Retrieve()
+        public IEnumerable<Vendor> Retrieve()
         {
             if(vendors == null)
             {
                 vendors = new List<Vendor>();
 
                 vendors.Add(new Vendor() { VendorId = 1, CompanyName="ABC Corp", Email="abc@abc.com"});
-                vendors.Add(new Vendor() { VendorId = 2, CompanyName = "XYZ Corp", Email = "xyz@xyz.com" });
+                vendors.Add(new Vendor() { VendorId = 2, CompanyName = "XYZ Inc", Email = "xyz@xyz.com" });
             }
-
 
             for (int i = 0; i < vendors.Count; i++)
             {
                 Console.WriteLine(vendors[i]);
             }
 
-            //foreach (var vendor in vendors)
-            //{
-            //    Console.WriteLine(vendor);
-            //}            
-
             return vendors;
         }
+
 
         /// <summary>
-        /// Retrieves all of the approved vendors
+        /// Retrieves all of the approved vendors, one at a time
         /// </summary>
         /// <returns></returns>
-        public Dictionary<string,Vendor> RetrieveWithKeys()
+        public IEnumerable<Vendor> RetrieveWithIterator()
         {
-            var vendors = new Dictionary<string, Vendor>()
+            //Get the data from the database
+            this.Retrieve();
+            foreach (var vendor in vendors)
             {
-                {"ABC Corp", new Vendor() {VendorId=5, CompanyName = "ABC Corp", Email = "abc@abc.com"} },
-                {"XYZ Inc", new Vendor() {VendorId=8, CompanyName = "XYZ Corp", Email = "xyz@xyz.com"} },
-            };
-
-            //foreach (var companyName in vendors.Keys)
-            //{
-            //    Console.WriteLine(vendors[companyName]);
-            //}
-
-            //if (vendors.ContainsKey("XYZ"))
-            //{
-            //    Console.WriteLine(vendors["XYZ"]);
-            //}
-
-            //Vendor vendor;
-            //if (vendors.TryGetValue("XYZ", out vendor))
-            //{
-            //    Console.WriteLine(vendors);
-            //}
-
-
-            //foreach (var vendor in vendors.Values)
-            //{
-            //    Console.WriteLine(vendor);
-            //}
-
-            foreach (var element in vendors)
-            {
-                var vendor = element.Value;
-                var key = element.Key;
-                Console.WriteLine($"Key: {key} Value: {vendor}");
+                Console.WriteLine($"Vendor Id: {vendor.VendorId}");
+                yield return vendor;
             }
+
+        }
+
+        public IEnumerable<Vendor> RetrieveAll()
+        {
+            var vendors = new List<Vendor>()
+            {
+                { new Vendor()
+                  { VendorId=1, CompanyName="ABC Corp", Email="abc@abc.com"} },
+                { new Vendor()
+                  { VendorId=2, CompanyName="XYZ Inc", Email="xyz@xyz.com"} },
+                { new Vendor()
+                  { VendorId=12, CompanyName="EFG Ltd", Email="efg@efg.com"} },
+                { new Vendor()
+                  { VendorId=17, CompanyName="HIJ AG", Email="hij@hij.com"} },
+                { new Vendor()
+                  { VendorId=22, CompanyName="Amalgamated Toys", Email="a@abc.com"} },
+                { new Vendor()
+                  { VendorId=28, CompanyName="Toy Blocks Inc", Email="blocks@abc.com"} },
+                { new Vendor()
+                  { VendorId=31, CompanyName="Home Products Inc", Email="home@abc.com"} },
+                { new Vendor()
+                  { VendorId=35, CompanyName="Car Toys", Email="car@abc.com"} },
+                { new Vendor()
+                  { VendorId=42, CompanyName="Toys for Fun", Email="fun@abc.com"} },
+            };
 
             return vendors;
         }
+
     }
 }
--- AcmeApp/Tests/Acme.BizTests/VendorRepositoryTests.cs
+++ AcmeApp/Tests/Acme.BizTests/VendorRepositoryTests.cs
@@ -48,32 +48,77 @@ namespace Acme.Biz.Tests
             var expected = new List<Vendor>();
 
             expected.Add(new Vendor() { VendorId = 1, CompanyName = "ABC Corp", Email = "abc@abc.com" });
-            expected.Add(new Vendor() { VendorId = 2, CompanyName = "XYZ Corp", Email = "xyz@xyz.com" });
+            expected.Add(new Vendor() { VendorId = 2, CompanyName = "XYZ Inc", Email = "xyz@xyz.com" });
 
             //Act
             var actual = repository.Retrieve();
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual.ToList());
+        }
+
+        [TestMethod()]
+        public void RetrieveWithIteratorTest()
+        {
+            //Arrange
+            var repository = new VendorRepository();
+            var expected = new List<Vendor>()
+            {
+                { new Vendor() { VendorId=1, CompanyName="ABC Corp", Email="abc@abc.com"} },
+                { new Vendor() { VendorId=2, CompanyName="XYZ Inc", Email="xyz@xyz.com"} }
+            };
+
+            //Act
+            var vendorIterator = repository.RetrieveWithIterator();
+            foreach (var item in vendorIterator)
+            {
+                Console.WriteLine(item);
+            }
+            var actual = vendorIterator.ToList();
 
             //Assert
             CollectionAssert.AreEqual(expected, actual);
         }
 
         [TestMethod()]
-        public void RetrieveWithKeysTest()
+        public void RetrieveAllTest()
         {
             //Arrange
             var repository = new VendorRepository();
-            var expected = new Dictionary<string, Vendor>()
+            var expected = new List<Vendor>()
             {
-                {"ABC Corp", new Vendor() {VendorId=5, CompanyName = "ABC Corp", Email = "abc@abc.com"} },
-                {"XYZ Inc", new Vendor() {VendorId=8, CompanyName = "XYZ Corp", Email = "xyz@xyz.com"} },
+                { new Vendor()
+                  { VendorId=22, CompanyName="Amalgamated Toys", Email="a@abc.com"} },
+                { new Vendor()
+                  { VendorId=35, CompanyName="Car Toys", Email="car@abc.com"} },
+                { new Vendor()
+                  { VendorId=28, CompanyName="Toy Blocks Inc", Email="blocks@abc.com"} },
+                { new Vendor()
+                  { VendorId=42, CompanyName="Toys for Fun", Email="fun@abc.com"} }
             };
 
             //Act
-            var actual = repository.RetrieveWithKeys();
+            var vendors = repository.RetrieveAll();
+
+            //var vendorQuery = from v in vendors
+            //                  where v.CompanyName.Contains("Toy")
+            //                  orderby v.CompanyName
+            //                  select v;
+
+            //var vendorQuery = vendors.Where(FilterCompanies)
+            //                         .OrderBy(OrderCompaniesByName);
+            var vendorQuery = vendors.Where(v => v.CompanyName.Contains("Toy"))
+                                     .OrderBy(v => v.CompanyName);
+
+
 
             //Assert
-            CollectionAssert.AreEqual(expected, actual);
+            CollectionAssert.AreEqual(expected, vendorQuery.ToList());
         }
+
+        //private bool FilterCompanies(Vendor v) => v.CompanyName.Contains("Toy");
+
+        //private string OrderCompaniesByName(Vendor v) => v.CompanyName;
 
 
         //[TestMethod()]
